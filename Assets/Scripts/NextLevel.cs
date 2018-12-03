aa7dccb8e212ade266a3597f4476a79469c3671a
Jo Brown <joebobjrjr@gmail.com>
2018-12-03 11:57:51 -0500

    Haha

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            int current = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(current + 1);
+        }
+    }
+}
